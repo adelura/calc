cc4e03207ee6bedbae5658381c3d43065612e5ae
adelura <adelura@gmail.com>
2017-11-27 23:28:05 +0000

    Separated transactions

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
 import { calculateTransactions } from './calculator.js';
-
-let peoples = {
-    A: 'A',
-    B: 'B',
-    C: 'C',
-    D: 'D',
-    M: 'M',
-    T: 'T',
-};
-
-// let transactions = [
-//     { amount: 10,   payed: peoples.A,    borrower: peoples.M },
-//     { amount: 5,    payed: peoples.A,    borrower: peoples.T },
-//     { amount: 3,    payed: peoples.M,    borrower: peoples.T },
-//     { amount: 2,    payed: peoples.T,    borrower: peoples.A },
-// ];
-let transactions = [
-    { amount: 8,    payed: peoples.A,    borrower: peoples.C },
-    { amount: 7,    payed: peoples.A,    borrower: peoples.D },
-    { amount: 2,    payed: peoples.B,    borrower: peoples.A },
-];
+import transactions from "./transactions.js";
 
 console.table(transactions);
 
